c264b0064dc2d4440ad8baa24bfca6377207e0d2
vjrj <vjrj@ourproject.org>
2021-03-21 19:37:05 +0100

    Follow redirects and better error handling

--- api/controllers/image-proxy.js
+++ api/controllers/image-proxy.js
@@ -1,8 +1,28 @@
 const bent = require('bent');
 const getBuffer = bent('buffer');
+const { http, https } = require('follow-redirects');
 
 module.exports = async function proxy(req, res) {
   var url = req.params[0];
-  let buffer = await getBuffer(url);
-  return res.send(buffer);
+  (url.indexOf('http://') === 0 ? http : https)
+    .get(url, async (response) => {
+      let buffer = await getBuffer(response.responseUrl);
+      return res.send(buffer);
+    })
+    .on('error', (e) => {
+      switch (e.code) {
+        case 'ETIMEDOUT':
+          res.status(408);
+          break;
+        case 'ENOTFOUND':
+          res.status(404);
+          break;
+        default:
+          res.status(500);
+      }
+      console.log(`code: ${e.code}`);
+      console.log(`name: ${e.name}`);
+      console.log(`message: ${e.message}`);
+      res.send('Error retrieving the image');
+    });
 };
