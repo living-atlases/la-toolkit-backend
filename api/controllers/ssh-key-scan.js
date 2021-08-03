c89a799688a0c36dca36cfcd7361ea28bcde318e
vjrj <vjrj@ourproject.org>
2021-08-03 10:12:17 +0200

    Scan pem private keys too

--- api/controllers/ssh-key-scan.js
+++ api/controllers/ssh-key-scan.js
@@ -1,10 +1,12 @@
 const cp = require('child_process');
 const fs = require('fs');
-const { defExecTimeout } = require('../libs/utils.js');
+const path = require("path");
+const {defExecTimeout} = require('../libs/utils.js');
 
 const sshKeyScan = () => {
+  let sailsConfigSshDir = sails.config.sshDir;
   let out = cp.execSync(
-    `for i in $(ls ${sails.config.sshDir}*pub) ; do N=$(basename $i .pub); echo -n "$Nł"; grep -q ENCRYPTED ${sails.config.sshDir}/$N; if [[ $? -eq 0 ]]; then echo -n "1ł"; else echo -n "0ł"; fi; ssh-keygen -l -f ${sails.config.sshDir}/$N | sed 's/ /ł/1' | sed 's/ /ł/1' ; done;`,
+    `for i in $(ls ${sailsConfigSshDir}*pub) ; do N=$(basename $i .pub); NP=$(ls -1 $N* | grep -v 'pub$' | head -1); echo -n $NP"ł"; grep -q ENCRYPTED ${sailsConfigSshDir}/$NP; if [[ $? -eq 0 ]]; then echo -n "1ł"; else echo -n "0ł"; fi; ssh-keygen -l -f ${sailsConfigSshDir}/$N.pub | sed 's/ /ł/1' | sed 's/ /ł/1' ; done;`,
     {
       shell: '/bin/bash',
       cwd: sails.config.sshDir,
@@ -33,12 +35,14 @@ module.exports = {
           let n = key.lastIndexOf(' ');
           key = key.slice(0, n) + key.slice(n).replace(' ', 'ł');
           let kS = key.split('ł');
+          let privExt = path.extname(`${sails.config.sshDir}${kS[0]}`);
+          let privExtBaseName = path.basename(`${sails.config.sshDir}${kS[0]}`, privExt);
           let k = {
             name: `${kS[0]}`,
             encrypted: parseInt(kS[1]) === 1,
             size: parseInt(kS[2]),
             publicKey: fs
-              .readFileSync(`${sails.config.sshDir}${kS[0]}.pub`, 'utf8')
+              .readFileSync(`${sails.config.sshDir}${privExtBaseName}.pub`, 'utf8')
               .replace('\n', ''),
             fingerprint: `${kS[3]}`,
             desc: `${kS[4]}`,
@@ -48,7 +52,7 @@ module.exports = {
           keys.push(k);
         }
       });
-      let resultJson = { keys: keys };
+      let resultJson = {keys: keys};
       // console.log(resultJson);
       return this.res.json(resultJson);
     }
