c26bda04dd56a53d575c5d57b9b07927b1e825f0
vjrj <vjrj@ourproject.org>
2021-08-25 15:59:17 +0200

    Correct populate call

--- api/controllers/delete-project.js
+++ api/controllers/delete-project.js
@@ -14,16 +14,16 @@ module.exports = {
   exits: {},
 
   fn: async function (inputs) {
-    await CmdHistoryEntry.destroy({ projectId: inputs.id });
-    await ServiceDeploy.destroy({ projectId: inputs.id });
-    await Service.destroy({ projectId: inputs.id });
-    await Server.destroy({ projectId: inputs.id });
-    await Variable.destroy({ projectId: inputs.id });
-    await Project.destroy({ id: inputs.id }).meta({
+    await CmdHistoryEntry.destroy({projectId: inputs.id});
+    await ServiceDeploy.destroy({projectId: inputs.id});
+    await Service.destroy({projectId: inputs.id});
+    await Server.destroy({projectId: inputs.id});
+    await Variable.destroy({projectId: inputs.id});
+    await Project.destroy({id: inputs.id}).meta({
       // This seems that does not work so we delete the associations before
       cascade: true,
     });
-    let projects = await sails.helpers.populateProject;
-    return this.res.json({ projects: projects });
+    let projects = await sails.helpers.populateProject();
+    return this.res.json({projects: projects});
   },
 };
