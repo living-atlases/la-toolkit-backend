1257f578d2a071172ba13a49d3595749e94b2c21
vjrj <vjrj@ourproject.org>
2022-01-21 11:28:23 +0100

    Take into account multiple artifacts for a same service like (collector/ala-collectory)

--- api/controllers/get-deps-versions.js
+++ api/controllers/get-deps-versions.js
@@ -74,43 +74,53 @@ module.exports = {
       depList.map(async (service) => {
         result[service] = {};
         for (let repo of ["releases", "snapshots"]) {
-          let artifact = inputs.deps[service];
-          try {
-            if (artifact === 'solr' || artifact === 'solrcloud') {
-              // As solr does not provide a list of versions, we maintain this json in github :-/
-              const solrData = await request('https://raw.githubusercontent.com/living-atlases/la-toolkit-backend/master/assets/solr-versions.json');
-              result[service][repo] = JSON.parse(solrData);
-            } else if (artifact === 'pipelines') {
-              // apt install la-pipelines=2.9.9-SNAPSHOT\*
-              // apt-cache madison la-pipelines  | cut -d"|" -f 1,2 | cut -d"+" -f 1 | sort -r | uniq
-              // Other option but does not match la-pipelines releases:
-              // let pipelinesUrl = 'https://api.github.com/repos/gbif/pipelines/tags';
-              // console.log(`${artifact} ${versions}`);
-              result[service][repo] = await pipelinesVersions();
-            } else {
-              let artifactConv = artifact === 'ala-namematching-server' ? 'names/ala-namematching-server' : artifact;
-              let nexusUrl = `https://nexus.ala.org.au/service/local/repositories/${repo}/content/au/org/ala/${artifactConv}/maven-metadata.xml`;
+          let artifacts = inputs.deps[service];
+          // multiple artifacts for the same service can be specified separated by commas, like "collectory ala-collectory"
+          for (let artifact of artifacts.split(' ')) {
+            try {
+              if (artifact === 'solr' || artifact === 'solrcloud') {
+                // As solr does not provide a list of versions, we maintain this json in github :-/
+                const solrData = await request('https://raw.githubusercontent.com/living-atlases/la-toolkit-backend/master/assets/solr-versions.json');
+                result[service][repo] = JSON.parse(solrData);
+              } else if (artifact === 'pipelines') {
+                // apt install la-pipelines=2.9.9-SNAPSHOT\*
+                // apt-cache madison la-pipelines  | cut -d"|" -f 1,2 | cut -d"+" -f 1 | sort -r | uniq
+                // Other option but does not match la-pipelines releases:
+                // let pipelinesUrl = 'https://api.github.com/repos/gbif/pipelines/tags';
+                // console.log(`${artifact} ${versions}`);
+                result[service][repo] = await pipelinesVersions();
+              } else {
+                let artifactConv = artifact === 'ala-namematching-server' ? 'names/ala-namematching-server' : artifact;
+                let nexusUrl = `https://nexus.ala.org.au/service/local/repositories/${repo}/content/au/org/ala/${artifactConv}/maven-metadata.xml`;
 
-              const xmlData = await request(nexusUrl);
-              // https://nexus.ala.org.au/service/local/repositories/releases/content/au/org/ala/ala-hub/4.0.8/ala-hub-4.0.8.war
-              // https://nexus.ala.org.au/service/local/repositories/snapshots/content/au/org/ala/ala-hub/maven-metadata.xml
-              if (parser.validate(xmlData) === true) { //optional (it'll return an object in case it's not valid)
-                // console.log(`${artifact} ${JSON.stringify(jsonObj)}`);
-                result[service][repo] =
-                  parser.parse(xmlData, {
+                const xmlData = await request(nexusUrl);
+                // https://nexus.ala.org.au/service/local/repositories/releases/content/au/org/ala/ala-hub/4.0.8/ala-hub-4.0.8.war
+                // https://nexus.ala.org.au/service/local/repositories/snapshots/content/au/org/ala/ala-hub/maven-metadata.xml
+                if (parser.validate(xmlData) === true) { //optional (it'll return an object in case it's not valid)
+                  let jsonObj = parser.parse(xmlData, {
                     numParseOptions: {
                       skipLike: /[0-9.]*/
                     }
                   });
+                  if (result[service][repo] != null) {
+                    result[service][repo]['metadata']['versioning']['versions']['version'] = [...result[service][repo]['metadata']['versioning']['versions']['version'], ...jsonObj['metadata']['versioning']['versions']['version']]
+                  } else {
+                    result[service][repo] = jsonObj;
+                  }
+                  // if (service === 'collectory' || service === 'ala-collectory') console.log(`${service} (${repo}): artifact: ${artifact} ${JSON.stringify(result[service][repo])}`);
+                }
               }
+            } catch (e) {
+              // console.log(`url: ${nexusUrl}`);
+              console.log(`message: ${e.message}`);
+              throw "getError";
             }
-          } catch (e) {
-            // console.log(`url: ${nexusUrl}`);
-            console.log(`message: ${e.message}`);
-            throw "getError";
           }
         }
-      }))
+      }));
+    // Here we exclude versions that for some reason (bugs, old, security issues) we don't want to show to the user in the UI software selector
+    const excludeList = await request('https://raw.githubusercontent.com/living-atlases/la-toolkit-backend/master/assets/soft-versions-exclude-list.json');
+    result['excludeList'] = JSON.parse(excludeList);
     return this.res.json(result);
   }
 }
