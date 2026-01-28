bad419743c986f57d27bf87cffd96d7071a778c8
vjrj <vjrj@comunes.org>
2026-01-28 22:05:05 +0100

    Delete get-system-status controller function

--- api/controllers/get-system-status.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function getSystemStatus(req, res) {
-  return res.json({
-    dbUpgradeRequired: sails.config.custom.dbUpgradeRequired || false,
-    appName: 'Living Atlases Toolkit',
-    // We can add more status info here in the future
-  });
-};
